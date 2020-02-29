cfbea4f54be4345ca7abab8618cc5b7b8fd6ece6
Dimas0075 <49650266+Dimas0075@users.noreply.github.com>
2020-02-29 20:36:34 +0700

    Update soal3c.sh

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -1,3 +1,35 @@
+tempat=`pwd`
+filter=`ls $tempat | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
+
+if [[ ! -d "$tempat/kenangan" ]]
+then
+        mkdir $tempat/kenangan
+fi
+
+if [[ ! -d "$tempat/duplicate" ]]
+then
+        mkdir $tempat/duplicate
+fi
+
+if [[ $filter =~ [^0-9] ]]
+then
+        filter=0
+fi
+
+pertama=`expr $filter + 1`
+terakhir=`expr $filter + 28`
+
+for ((a=pertama; a<=terakhir; a++));
+do
+        wget -O pdkt_kusuma_$a https://loremflickr.com/320/240/cat -o $tempat/wget.log
+done
+
+for ((a=1;a<=filter;a++))
+do
+        loc="`cat $PWD/wget.log | grep "Location:" | head -$a | tail -1 | cut -d " " -f 2`"
+        echo $loc >> location.log
+done
+
 temp=""
 for ((a=1;a<=filter;a++))
 do
@@ -16,3 +48,4 @@ done
 
 cat $PWD/wget.log >> $PWD/wget.log.bak
 rm $PWD/wget.log
+                                                                                                                                                                                          50,16         All
