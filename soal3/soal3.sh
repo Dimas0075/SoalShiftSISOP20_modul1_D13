61cf3b7a5ffe13e901e5ad813dd449379695d473
Dimas0075 <49650266+Dimas0075@users.noreply.github.com>
2020-02-29 20:10:33 +0700

    Update soal3.sh

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-for ((a=1;a<29;a++))
+
+tempat=`pwd`
+filter=`ls $tempat | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
+
+if [[ $filter =~ [^0-9] ]]
+then
+        filter=0
+fi
+
+pertama=`expr $filter + 1`
+terakhir=`expr $filter + 28`
+
+for ((a=pertama; a<=terakhir; a++));
 do
-        wget "https://loremflickr.com/320/240/cat" -O pdkt_kusuma_$a -o wget.log
+        wget https://loremflickr.com/320/240/cat -O pdkt_kusuma_$a -o $tempat/wget.log
 done
+
